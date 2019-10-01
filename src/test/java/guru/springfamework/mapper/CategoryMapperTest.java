46f0a528d35098f7370b74968797bf0cc63a04cb
germanheinz <heinz.german@gmail.com>
2019-10-01 19:34:24 +0200

    Test CategoryMapper

---
+++ src/test/java/guru/springfamework/mapper/CategoryMapperTest.java
@@ -0,0 +1,30 @@
+package guru.springfamework.mapper;
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.domain.Category;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+
+public class CategoryMapperTest {
+
+    CategoryMapper categoryMapper = guru.springfamework.mapper.CategoryMapper.INSTANCE;
+
+    @Test
+    public void categoryToCategoryDTO() {
+
+
+        Category category = new Category();
+        category.setId(1L);
+        category.setName("Joe");
+
+        CategoryDTO categoryDTO = categoryMapper.categoryToCategoryDTO(category);
+
+        assertEquals(Long.valueOf(1L), categoryDTO.getId());
+        assertEquals("Joe", categoryDTO.getName());
+
+
+    }
+}
